3dabaa6af095ae6fcf1f299bbd2e7bad21866b6a
Husam Al Jawaheri <hossamjawaheri@gmail.com>
2022-05-30 23:48:35 +0300

    Issue #20: Improved top links tooltip styling

--- web/src/components/Wallet/TopContent/Links/WalletTopContentLinks.js
+++ web/src/components/Wallet/TopContent/Links/WalletTopContentLinks.js
@@ -4,7 +4,8 @@ import { useHistory } from 'react-router-dom';
 
 // GraphVis
 import Graph from 'react-graph-vis';
-import 'vis-network/styles/vis-network.min.css';
+// import 'vis-network/styles/vis-network.min.css';
+import './vis-tooltip.css';
 
 // Redux
 import { useSelector } from 'react-redux';
